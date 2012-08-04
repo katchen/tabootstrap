bc7fd3352ab8c8c950034b1384c4292685f79ad5
seung lee <seung.j@Live.com>
2012-08-03 23:06:19 -0700

    setup

--- tabootstrap/tabootstrap/urls.py
+++ tabootstrap/tabootstrap/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tabootstrap.views.home', name='home'),
-    # url(r'^tabootstrap/', include('tabootstrap.foo.urls')),
+    url(r'^$','taboo.views.index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
