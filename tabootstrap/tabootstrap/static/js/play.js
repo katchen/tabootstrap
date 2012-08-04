e8eef0588708a4089d203079741d501271ba9eb1
Tanay Jaipuria <tanay46@hotmail.com>
2012-08-04 03:42:49 -0700

    Seperating files and shit

--- tabootstrap/tabootstrap/static/js/play.js
+++ tabootstrap/tabootstrap/static/js/play.js
@@ -1,4 +1,62 @@
 $(document).ready(function(){
+	var counter = 60;
+	function seconds_to_time(sec)
+    {
+        var minutes = (Math.floor(sec / 60)).toString();
+        var seconds = (sec % 60).toString();
+
+        if(seconds.length == 1)
+        {
+            seconds = "0" + seconds;
+        }
+
+        return minutes + ":" + seconds;
+    }
+
+    function getContent(startup,banned)
+    {
+
+        document.getElementById('startup').innerHTML = startup;
+        var banned = Array(1,2,3);
+        var wordlist = "";
+        for(i=0;i<banned.length;i++)
+        {
+          var element = "<li>";
+          element += banned[i];
+          element += "</li>";
+          wordlist+=element;
+        }
+        document.getElementById('banned').innerHTML = wordlist;
+        $('#banned').listview('refresh');
+    } 
+
+    function tick () {
+		if (counter>0)
+		{
+			counter-=1;
+    		setTimeout ('tick()', 1000);
+			$('#counter').html(counter);
+		}
+		else
+		{
+			counter = 60
+			$('#counter').html("done");
+			
+		}
+    	// if (document.getElementById('counter').data > 0) {
+    	// 	document.getElementById('counter').data = document.getElementById ('counter').firstChild.data - 1;
+    	// 	setTimeout ('tick()', 1000);
+    	// } else {
+    	// 	document.getElementById('counter').data = 'done';
+    	// }
+    }
+
+    if (document.getElementById('counter')) = function () {
+      console.log("HII");
+      document.getElementById('counter').data = 60;
+    	tick();
+    }
+    
     prev_words = [];
     $("#getNewSet").click(function(){
         getNewSet();
