5831c175e05d1b4cbffbcf98f19295f9e59f5429
seung lee <seung.j@Live.com>
2012-08-04 02:10:23 -0700

    ajax workin bro

--- tabootstrap/taboo/views.py
+++ tabootstrap/taboo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from taboo.models import *
 import random
+import json
 
 def index(request):
     return render_to_response('index.html')
@@ -11,17 +12,24 @@ def instructions(request):
 
 def play(request):
     if request.method=="GET" and request.is_ajax():
+        data = {'used_all':False}
+
         prev_words = request.GET['prev_words']
         prev_list = str(prev_words).split(",")
         taboo_words = TabooWord.objects.exclude(word__in=prev_list)
-        #if taboo_words.exists() == False:
-            #taboo_words = TabooWord.objects.all()
+        if taboo_words.exists() == False:
+            taboo_words = TabooWord.objects.all()
+            data['used_all'] = True
         taboo_word = random.choice(taboo_words)
         taboo_string = taboo_word.word
         banned_words = taboo_word.banned_words.all()
         banned_list = []
         for ban in banned_words:
             banned_list.append(ban.word)
-        return HttpResponse(banned_list)
+
+        data['taboo_word'] = taboo_string
+        data['banned_words'] = banned_list
+        data = json.dumps(data)
+        return HttpResponse(data)
 
     return render_to_response('play.html')
--- tabootstrap/tabootstrap/static/js/play.js
+++ tabootstrap/tabootstrap/static/js/play.js
@@ -1,25 +1,34 @@
 
 $(document).ready(function(){
-
+    prev_words = [];
     $("#getNewSet").click(function(){
-        getNewSet(["fe","twitter"]);
+        getNewSet(prev_words);
     })
 
     function getNewSet(prev_words){
-        prev_words = String(prev_words)
+        prev_words_string = String(prev_words)
         var data = {};
-        data['prev_words'] = prev_words;
+        data['prev_words'] = prev_words_string;
         var new_word;
         var new_banned_words;
         $.ajax({
-                   type: "GET",
-                   url: "./",
-                   data: data,
-                   success: function(word){
-                       alert("BRO")
-                       alert(word);
-                   }
-               })
+           type: "GET",
+           url: "./",
+           data: data,
+           success: function(data){
+               data_dict = eval("("+data+")")
+               new_word = data_dict['taboo_word']
+               new_banned_words = data_dict['banned_words']
+               used_all = data_dict['used_all']
+               if (used_all) {
+                   prev_words.length = 0
+               }
+               prev_words.push(new_word)
+               
+               alert(prev_words)
+               alert(new_word+": "+new_banned_words)
+           }
+        })
     }
 
 })
