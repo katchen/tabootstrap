3d2776664dcbaf88bf5ec80e3668ece8c31b38d1
Eric Xiao <exiao3@gmail.com>
2012-08-03 23:31:03 -0700

    Yup

--- tabootstrap/tabootstrap/settings.py
+++ tabootstrap/tabootstrap/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = SITE_ROOT + '/static_files/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,6 +69,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    SITE_ROOT + '/static/',
 )
 
 # List of finder classes that know how to find static files in
