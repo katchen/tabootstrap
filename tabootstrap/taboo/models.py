5da80645f92ce7618d9d39190492d9d33ca082f5
Eric Xiao <exiao3@gmail.com>
2012-08-03 23:03:16 -0700

    Models for taboo words

--- tabootstrap/taboo/models.py
+++ tabootstrap/taboo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class TabooWord(models.Model):
+    word = models.CharField(max_length=30)
+    banned_words = models.ManyToMany('BannedWord')
+    user_created = models.BooleanField(default=False)
+    
+class BannedWord(models.Model):
+    word = models.CharField(max_length=30)
+    
