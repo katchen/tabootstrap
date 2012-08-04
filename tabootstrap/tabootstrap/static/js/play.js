f93c9a7ce10ada19d2c8ab2976bac1f0453c6970
seung lee <seung.j@Live.com>
2012-08-04 02:40:32 -0700

    more ajax api

--- tabootstrap/tabootstrap/static/js/play.js
+++ tabootstrap/tabootstrap/static/js/play.js
@@ -1,34 +1,38 @@
-
 $(document).ready(function(){
     prev_words = [];
     $("#getNewSet").click(function(){
-        getNewSet(prev_words);
+        getNewSet();
     })
 
-    function getNewSet(prev_words){
+    function getNewSet(){
         prev_words_string = String(prev_words)
         var data = {};
         data['prev_words'] = prev_words_string;
-        var new_word;
-        var new_banned_words;
+        var return_dict = {};
         $.ajax({
            type: "GET",
            url: "./",
            data: data,
            success: function(data){
                data_dict = eval("("+data+")")
+               used_all = data_dict['used_all']
                new_word = data_dict['taboo_word']
                new_banned_words = data_dict['banned_words']
-               used_all = data_dict['used_all']
                if (used_all) {
                    prev_words.length = 0
                }
                prev_words.push(new_word)
+
+
+
+               // START DOING STUFF WITH DATA HERE
+               // new_word is a string of the new taboo word
+               // new_banned_word is an array of strings, which are the words not allowed to be used
+
                
-               alert(prev_words)
-               alert(new_word+": "+new_banned_words)
            }
         })
     }
+
 
 })
