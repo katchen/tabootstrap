c97832c34df45c19820bc952232bf765bd6d082e
seung lee <seung.j@Live.com>
2012-08-04 04:27:59 -0700

    Hi

--- tabootstrap/tabootstrap/static/js/play.js
+++ tabootstrap/tabootstrap/static/js/play.js
@@ -1,4 +1,5 @@
 var return_dict = {}
+var counter;
 
 $(document).ready(function(){
 	var counter = 60;
@@ -53,50 +54,75 @@ $(document).ready(function(){
     	// }
     }
 
-    if (document.getElementById('counter')) = function () {
-      console.log("HII");
-      document.getElementById('counter').data = 60;
-    	tick();
-    }
-    
-    prev_words = [];
-    var prev_words = [];
-    $("#getNewSet").click(function(){
-        getNewSet();
-    })
+//    if (document.getElementById('counter')) = function () {
+//      console.log("HII");
+//      document.getElementById('counter').data = 60;
+//    	tick();
+//    }
 
-    function getNewSet(){
-        prev_words_string = String(prev_words)
-        var data = {};
-        data['prev_words'] = prev_words_string;
-        $.ajax({
-           type: "GET",
-           url: "./",
-           data: data,
-           success: function(data){
-                data_dict = eval("("+data+")")
-                used_all = data_dict['used_all']
-                new_word = data_dict['taboo_word']
-                new_banned_words = data_dict['banned_words']
-                if (used_all) {
-                    prev_words.length = 0
-                }
-                prev_words.push(new_word)
-                setDict(new_word,new_banned_words);
-
-               // START DOING STUFF WITH DATA HERE
-               // new_word is a string of the new taboo word
-               // new_banned_word is an array of strings, which are the words not allowed to be used
-
-
-           }
+    $(document).ready(function(){
+        var prev_words = [];
+        $("#getNewSet").click(function(){
+            getNewSet();
         })
-    }
 
-    function setDict(new_word,new_banned_words) {
-        return_dict['taboo_word'] = new_word;
-        return_dict['new_banned_words'] = new_banned_words;
-        alert(return_dict['taboo_word'])
-    }
+        function getNewSet(){
+            prev_words_string = String(prev_words)
+            var data = {};
+            data['prev_words'] = prev_words_string;
+            $.ajax({
+                       type: "GET",
+                       url: "./",
+                       data: data,
+                       success: function(data){
+                           data_dict = eval("("+data+")")
+                           used_all = data_dict['used_all']
+                           new_word = data_dict['taboo_word']
+                           new_banned_words = data_dict['banned_words']
+                           if (used_all) {
+                               prev_words.length = 0
+                           }
+                           prev_words.push(new_word)
+                           setDict(new_word,new_banned_words);
+
+                           // START DOING STUFF WITH DATA HERE
+                           // new_word is a string of the new taboo word
+                           // new_banned_word is an array of strings, which are the words not allowed to be used
+
+
+                       }
+                   })
+        }
+
+        function setDict(new_word,new_banned_words) {
+            return_dict['taboo_word'] = new_word;
+            return_dict['new_banned_words'] = new_banned_words;
+            alert(return_dict['taboo_word'])
+        }
+
+        function startCounter(seconds) {
+            counter = seconds;
+            $("#counter").html(counter)
+            var interval = setInterval(function(){counter -= 1;
+                $("#counter").html(counter);if (counter == 0) {
+                    clearInterval(interval);
+                    $("#counter").html("Done")
+                }},1000);
+
+        }
+
+        function tick() {
+
+            counter -= 1;
+            $("#counter").html(counter)
+            if (counter == 0) {
+                clearInterval(interval)
+            }
+        }
+
+        $("#count").click( function() {
+            startCounter(3);
+        })
+    })
 
 })
